a5ceaf1ff8de5e1aae34611c13797e1301b23731
Kavana S <Kavana.S@sonata-software.com>
2023-03-30 14:58:40 +0530

    first push

---
+++ src/Arrayinsert.java
@@ -0,0 +1,14 @@
+
+public class Arrayinsert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]=new int[4];
+a[0]=10;
+a[1]=20;
+a[2]=30;
+a[3]=40;
+
+	}
+}
+
